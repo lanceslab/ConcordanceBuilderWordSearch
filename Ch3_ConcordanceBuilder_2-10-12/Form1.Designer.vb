d236b22d19921a78105267297734e01a1fa328b0
LancesLab <lanceandsheri@yahoo.com>
2019-08-04 13:58:39 -0400

    Project upload

---
+++ Ch3_ConcordanceBuilder_2-10-12/Form1.Designer.vb
@@ -0,0 +1,145 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStripDropDownButton1 = New System.Windows.Forms.ToolStripDropDownButton()
+        Me.mnuFileOpen = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuFileExit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.lblFileName = New System.Windows.Forms.ToolStripLabel()
+        Me.txtSearchWord = New System.Windows.Forms.ToolStripTextBox()
+        Me.btnFind = New System.Windows.Forms.ToolStripButton()
+        Me.btnAll = New System.Windows.Forms.ToolStripButton()
+        Me.lstBox = New System.Windows.Forms.ListBox()
+        Me.ofdOpenFile = New System.Windows.Forms.OpenFileDialog()
+        Me.ToolStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripDropDownButton1, Me.lblFileName, Me.txtSearchWord, Me.btnFind, Me.btnAll})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(940, 27)
+        Me.ToolStrip1.TabIndex = 0
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'ToolStripDropDownButton1
+        '
+        Me.ToolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ToolStripDropDownButton1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuFileOpen, Me.mnuFileExit})
+        Me.ToolStripDropDownButton1.Image = CType(resources.GetObject("ToolStripDropDownButton1.Image"), System.Drawing.Image)
+        Me.ToolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripDropDownButton1.Name = "ToolStripDropDownButton1"
+        Me.ToolStripDropDownButton1.Size = New System.Drawing.Size(46, 24)
+        Me.ToolStripDropDownButton1.Text = "File"
+        '
+        'mnuFileOpen
+        '
+        Me.mnuFileOpen.Name = "mnuFileOpen"
+        Me.mnuFileOpen.Size = New System.Drawing.Size(120, 26)
+        Me.mnuFileOpen.Text = "Open"
+        '
+        'mnuFileExit
+        '
+        Me.mnuFileExit.Name = "mnuFileExit"
+        Me.mnuFileExit.Size = New System.Drawing.Size(120, 26)
+        Me.mnuFileExit.Text = "Exit"
+        '
+        'lblFileName
+        '
+        Me.lblFileName.Name = "lblFileName"
+        Me.lblFileName.Size = New System.Drawing.Size(118, 24)
+        Me.lblFileName.Text = "Click File to start"
+        '
+        'txtSearchWord
+        '
+        Me.txtSearchWord.Name = "txtSearchWord"
+        Me.txtSearchWord.Size = New System.Drawing.Size(132, 27)
+        '
+        'btnFind
+        '
+        Me.btnFind.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.btnFind.Image = CType(resources.GetObject("btnFind.Image"), System.Drawing.Image)
+        Me.btnFind.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.btnFind.Name = "btnFind"
+        Me.btnFind.Size = New System.Drawing.Size(41, 24)
+        Me.btnFind.Text = "Find"
+        '
+        'btnAll
+        '
+        Me.btnAll.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.btnAll.Image = CType(resources.GetObject("btnAll.Image"), System.Drawing.Image)
+        Me.btnAll.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.btnAll.Name = "btnAll"
+        Me.btnAll.Size = New System.Drawing.Size(31, 24)
+        Me.btnAll.Text = "All"
+        '
+        'lstBox
+        '
+        Me.lstBox.ColumnWidth = 100
+        Me.lstBox.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.lstBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstBox.FormattingEnabled = True
+        Me.lstBox.ItemHeight = 22
+        Me.lstBox.Location = New System.Drawing.Point(0, 27)
+        Me.lstBox.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.lstBox.Name = "lstBox"
+        Me.lstBox.Size = New System.Drawing.Size(940, 373)
+        Me.lstBox.TabIndex = 1
+        '
+        'ofdOpenFile
+        '
+        Me.ofdOpenFile.FileName = "OpenFileDialog1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(940, 400)
+        Me.Controls.Add(Me.lstBox)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Lance'sLab - Searching for a word in a Text file"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ToolStrip1 As System.Windows.Forms.ToolStrip
+    Friend WithEvents ToolStripDropDownButton1 As System.Windows.Forms.ToolStripDropDownButton
+    Friend WithEvents mnuFileOpen As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuFileExit As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents lblFileName As System.Windows.Forms.ToolStripLabel
+    Friend WithEvents txtSearchWord As System.Windows.Forms.ToolStripTextBox
+    Friend WithEvents btnFind As System.Windows.Forms.ToolStripButton
+    Friend WithEvents btnAll As System.Windows.Forms.ToolStripButton
+    Friend WithEvents lstBox As System.Windows.Forms.ListBox
+    Friend WithEvents ofdOpenFile As System.Windows.Forms.OpenFileDialog
+
+End Class
---
+++ Ch3_ConcordanceBuilder_2-10-12/Form1.vb
@@ -0,0 +1,116 @@
+﻿
+
+Imports System.IO
+
+Public Class Form1
+
+    Private infile As StreamReader
+    Private wordDict As New Dictionary(Of String, List(Of Integer))
+    ' The rawText variable holds all input lines from the file, so we can display
+    ' these lines later in the list box when the user searches for a word.
+    Private rawText As New List(Of String)
+
+    ' Add the OpenInputFile function, which returns True if a file was selected by
+    ' the user. We have already discussed the mechanics of the OpenFileDialog.
+    Private Function OpenInputFile() As Boolean
+        ' Displays an OpenFileDialog control and lets the user
+        ' select the input file.
+        With ofdOpenFile
+            .InitialDirectory = Directory.GetCurrentDirectory()
+            .FileName = "*.txt"
+            '.FileName = "*.pdf"
+            Dim result As DialogResult = .ShowDialog()
+            If result = DialogResult.OK Then
+                infile = New StreamReader(.OpenFile())
+                ' the next line copies the filename from the OpenFileDialog control into a label on the tool strip
+                ' The call to Path.GetFileName returns just the filename after stripping off the long directory path
+                lblFileName.Text = "Filename: " & Path.GetFileName(.FileName)
+                Return True
+            Else
+                Return False
+            End If
+        End With
+    End Function
+
+
+
+
+    Private Sub mnuFileOpen_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles mnuFileOpen.Click
+        ' The user has clicked the File | Open menu item.
+        'calls OpenInputFile, and exits if the user canceled the dialog.
+        If Not OpenInputFile() Then Return
+
+        ' Read each line and insert each new word into the dictionary.
+        ' For each existing word, add its line number to the list for that word entry.
+        ' 
+        ' creates a variable named linenum, which will keep track of the line read most recently from the file.
+        Dim lineNum As Integer = 0
+        Dim filterOut As Char() = ("?':;.,!""").ToCharArray()
+
+        ' repeats the loop until the end of the input file.
+        Do While Not infile.EndOfStream()
+            ' reads a complete line of input from the file
+            Dim temp As String = infile.ReadLine()
+            If temp.Trim().Length = 0 Then Continue Do ' skips if the line is blank & goes back to the Do While
+
+            ' adds the line to the rawText list we declared earlier
+            rawText.Add(temp)
+            ' splits the input line into an array of words
+            Dim words() As String = temp.Split(" "c)
+
+            ' begins a new loop whose job is to take each word in the array, trim it and convert it to lowercase
+            For Each word As String In words
+                word = word.TrimEnd(filterOut).ToLower() ' convert it to lowercase
+                If word.Length > 0 AndAlso Char.IsLetter(word(0)) Then 'make sure it is not blank and it begins w/a letter
+                    ' we check if the word is already in the dictionary.
+                    If Not wordDict.ContainsKey(word) Then ' If it is not, we add the word to the dictionary
+                        ' and give it a new empty line number list.
+                        wordDict.Add(word, New List(Of Integer))
+                    End If
+                    ' we add the current line number to the list associated with the current word.
+                    wordDict(word).Add(lineNum)
+                End If
+            Next
+            ' increments the line number, and the loop goes back to read another line from the file
+            lineNum += 1
+        Loop
+    End Sub
+
+
+    Private Sub btnAll_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAll.Click
+
+        lstBox.MultiColumn = True
+        lstBox.Items.Clear()
+        For Each entry As KeyValuePair(Of String, List(Of Integer)) In wordDict
+            lstBox.Items.Add(entry.Key)
+        Next
+    End Sub
+    ' Notice how the code sets the MultiColumn property in the list box to display
+    ' as many words as possible at the same time.
+
+
+
+    Private Sub btnFind_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnFind.Click
+        ' Its job is to look for the user’s word in the dictionary and then pull out all
+        ' matching lines from the "rawText" list.
+
+        lstBox.MultiColumn = False
+        lstBox.Items.Clear()
+        ' calls ContainsKey to find out if the word (in txtSearchWord.Text) exists in the dictionary.
+        If wordDict.ContainsKey(txtSearchWord.Text) Then
+            ' if the word is found, the following expression returns a List(Of Integer) object containing
+            ' the line numbers where the word was found in the input file: ----- wordDict(txtSearchWord.
+            For Each lineNum As Integer In wordDict(txtSearchWord.Text)
+                ' uses each line number as an index into the rawText List object, and inserts the text into the lstbox
+                lstBox.Items.Add(lineNum & ":" & vbTab & rawText(lineNum))
+            Next
+        Else
+            lstBox.Items.Add("(word not found)")
+        End If
+    End Sub
+
+
+    Private Sub mnuFileExit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles mnuFileExit.Click
+        Me.Close()
+    End Sub
+End Class
